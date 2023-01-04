c6fc6cd0492a06dd837525ce2a2c109d1d36c4a4
JamesRunnalls <james.runnalls@eawag.ch>
2023-01-04 18:19:01 +0100

    Working prototype

--- routes/gitclone.js
+++ routes/gitclone.js
@@ -5,6 +5,7 @@ const format = require("pg-format");
 const fs = require("fs");
 const { spawn } = require("child_process");
 const db = require("../db");
+const path = require('path');
 var AWS = require("aws-sdk");
 const { checkObject, error, logging } = require("../functions");
 
@@ -47,176 +48,6 @@ router.get("/status/:id", async (req, res, next) => {
     return next(error(404, "Dataset not found in database"));
   }
   res.status(200).send(rows[0]);
-});
-
-router.post("/temp/", async (req, res, next) => {
-  // Verify inputs
-  var attributes = {
-    id: "integer",
-    ssh: "varchar",
-    dir: "varchar",
-    branch: "varchar",
-    file: "varchar",
-  };
-  var files = fs.readdirSync("git");
-  var request = req.body;
-  const { id, ssh, dir, branch, file } = request;
-  if (typeof request !== "object" || request === null) {
-    return next(error(400, "Request body must be an object"));
-  }
-  if (!checkObject(attributes, request)) {
-    return next(error(400, "Malformed request"));
-  }
-  if (files.includes(id.toString())) {
-    return next(error(400, "Malformed request"));
-  }
-  if (branch !== "master") {
-    return next(error(400, "Must be on the master branch"));
-  }
-  if (!isInt(id)) {
-    return next(error(400, "ID must be an integer"));
-  }
-  if (!verifySSH(ssh)) {
-    return next(error(400, "Malformed request"));
-  }
-
-  logger("post", "gitclone", "Processing gitclone for repo: " + ssh);
-
-  // Check if repository in database
-  var repo_id;
-  var { rows } = await db.query(
-    "SELECT * FROM repositories WHERE ssh = $1 AND branch = $2",
-    [ssh, branch]
-  );
-  if (rows.length > 0) {
-    repo_id = rows[0].id;
-  } else {
-    var { rows } = await db.query(
-      "INSERT INTO repositories (ssh,branch) VALUES ($1, $2) RETURNING *",
-      [ssh, branch]
-    );
-    repo_id = rows[0].id;
-  }
-
-  // Check if repository has been cloned
-  var repo_name = ssh.split("/")[1].split(".")[0];
-  var mode = "new";
-  if (fs.existsSync(`git/${repo_id}/${repo_name}`)) {
-    mode = "existing";
-  }
-
-  // Load git command
-  var gitCommand;
-  if (mode === "new") {
-    logger("post", "gitclone", "Cloning new repository", (indent = 1));
-    gitCommand =
-      `mkdir git/${repo_id} ` +
-      `&& cd git/${repo_id} ` +
-      `&& git clone --depth 1 ${ssh} -b ${branch}`;
-  } else {
-    logger("post", "gitclone", "Pulling existing repository", (indent = 1));
-    gitCommand = `cd git/${repo_id}/${repo_name} && git stash && git pull`;
-  }
-
-  // Handle clone status
-  var { rows: clonestatus } = await db.query(
-    "INSERT INTO clonestatus (status,message,repositories_id) VALUES ($1, $2, $3) RETURNING *",
-    ["inprogress", "Starting repository clone", repo_id]
-  );
-  var clonestatus_id = clonestatus[0].id;
-  var log = [];
-
-  // Send response to client
-  res.status(200).send({ clonestatus_id });
-
-  // Run the server commands
-  const child = spawn(gitCommand, {
-    shell: true,
-  });
-
-  child.stdout.on("data", (data) => {
-    db.query("UPDATE clonestatus SET message = $1 WHERE id = $2", [
-      data,
-      clonestatus_id,
-    ]);
-    log.push(`stdout: ${data}`);
-    logger("post", "gitclone", `stdout: ${data}`, (indent = 2));
-  });
-  child.stderr.on("data", (data) => {
-    db.query("UPDATE clonestatus SET message = $1 WHERE id = $2", [
-      data,
-      clonestatus_id,
-    ]);
-    log.push(`stdout: ${data}`);
-    console.error(`stdout: ${data}`);
-  });
-  child.on("error", async (data) => {
-    if (mode === "new") {
-      await db.query("DELETE FROM repositories WHERE id = $1", [repo_id]);
-    }
-    await db.query(
-      "UPDATE clonestatus SET status = $1, message = $2 WHERE id = $3",
-      ["failed", log, clonestatus_id]
-    );
-  });
-  child.on("exit", async (data) => {
-    try {
-      if (data === 0) {
-        logger(
-          "post",
-          "gitclone",
-          "Successfully cloned/ updated.",
-          (indent = 1)
-        );
-        // Discover files and filter for only NetCDF files
-        var files = fs.readdirSync(`git/${repo_id}/${dir}`);
-        files = files.filter((f) => f.split(".")[1] === "nc");
-
-        // Files in folder
-        var values = files.map((f) => [id, `git/${repo_id}/${dir}/${f}`, "nc"]);
-
-        var query = format(
-          "INSERT INTO files (datasets_id, filelink, filetype) VALUES %L RETURNING *",
-          values
-        );
-        var { rows: outObj } = await db.query(query).catch((e) => {
-          next(error(500, e));
-        });
-
-        var outFile = outObj.find(
-          (f) => f.filelink === `git/${repo_id}/${dir}/${file}`
-        );
-        var allFiles = getFiles("git/" + repo_id);
-
-        var output = {
-          repo_id: repo_id,
-          file: outFile,
-          files: outObj,
-          log: log,
-          allFiles: allFiles,
-        };
-
-        await db.query(
-          "UPDATE clonestatus SET message = $1, status = $2 WHERE id = $3",
-          [output, "succeeded", clonestatus_id]
-        );
-      } else {
-        if (mode === "new") {
-          await db.query("DELETE FROM repositories WHERE id = $1", [repo_id]);
-        }
-        await db.query(
-          "UPDATE clonestatus SET status = $1, message = $2 WHERE id = $3",
-          ["failed", log, clonestatus_id]
-        );
-      }
-    } catch (error) {
-      console.log(error.message);
-      await db.query(
-        "UPDATE clonestatus SET status = $1, message = $2 WHERE id = $3",
-        ["failed", error.message, clonestatus_id]
-      );
-    }
-  });
 });
 
 router.post("/", async (req, res, next) => {
@@ -401,6 +232,187 @@ router.post("/", async (req, res, next) => {
   });
 });
 
+router.post("/files/", async (req, res, next) => {
+  var request = req.body;
+  const { id, folder, ssh } = request;
+  if (typeof request !== "object" || request === null) {
+    return next(error(400, "Request body must be an object"));
+  }
+  if (folder === path.basename(folder)) {
+    return next(error(400, "Folder must be a path"));
+  }
+  if (!verifySSH(ssh)) {
+    return next(error(400, "Malformed request"));
+  }
+
+  logger("post", "gitclone/files", "Processing files from folder: " + folder);
+
+  var folder_list = folder.split("/");
+  var repo_id = folder_list[1];
+  var repo_name = folder_list[2];
+  var folder_path = folder_list.slice(3, folder_list.length).join("/");
+  var download_files = false;
+
+  if (fs.existsSync(`git/${repo_id}/${repo_name}/.bucket`)) {
+    logger("post", "gitclone/files", "Processing cloud storage files");
+    download_files = true;
+    const bucket = fs.readFileSync(
+      `git/${repo_id}/${repo_name}/.bucket`,
+      "utf8"
+    );
+    var bucket_name = bucket.replace("https://", "").split(".")[0];
+    var remote = ssh.split("@")[1].split(".git", 1)[0].split(":");
+    var host = remote[0];
+    var group = remote[1].split("/")[0];
+    var repository = remote[1].split("/")[1];
+
+    const s3 = new AWS.S3();
+
+    const params = {
+      Bucket: bucket_name,
+      Prefix: `${host}/${group}/${repository}/${folder_path}`,
+      MaxKeys: 1000,
+    };
+
+    bucket_files = await new Promise((resolve, reject) => {
+      try {
+        const allKeys = [];
+        listAllKeys();
+        function listAllKeys() {
+          s3.listObjectsV2(params, function (err, data) {
+            if (err) {
+              reject(err);
+            } else {
+              var contents = data.Contents;
+              contents.forEach(function (content) {
+                if (content.Key.slice(-3) == ".nc") {
+                  let file = content.Key.replace(
+                    `${host}/${group}`,
+                    `git/${repo_id}`
+                  );
+                  if (!allKeys.includes(file)) {
+                    allKeys.push(file);
+                  }
+                }
+              });
+              if (data.IsTruncated) {
+                params.ContinuationToken = data.NextContinuationToken;
+                listAllKeys();
+              } else {
+                resolve(allKeys);
+              }
+            }
+          });
+        }
+      } catch (e) {
+        reject(e);
+      }
+    });
+
+    var values = bucket_files.map((f) => [id, f, "nc"]);
+    var query = format(
+      "INSERT INTO files (datasets_id, filelink, filetype) VALUES %L RETURNING *",
+      values
+    );
+    var { rows: outObj } = await db.query(query).catch((e) => {
+      next(error(500, e));
+    });
+
+    var file = outObj[0];
+
+    // Handle status
+    var { rows: status } = await db.query(
+      "INSERT INTO clonestatus (status,message,repositories_id) VALUES ($1, $2, $3) RETURNING *",
+      ["inprogress", `Starting folder sync from ${bucket}`, repo_id]
+    );
+    var status_id = status[0].id;
+    var log = [];
+
+    res.status(200).send({ status_id, file, files: outObj });
+  } else if (fs.existsSync(folder)) {
+    var files = fs.readdirSync(folder);
+    files = files.filter((f) => f.split(".")[1] === "nc");
+    if (files.length > 0) {
+      var values = files.map((f) => [id, `${folder}/${f}`, "nc"]);
+      var query = format(
+        "INSERT INTO files (datasets_id, filelink, filetype) VALUES %L RETURNING *",
+        values
+      );
+      var { rows: outObj } = await db.query(query).catch((e) => {
+        next(error(500, e));
+      });
+
+      var file = outObj[0];
+      res.status(200).send({ file, files: outObj });
+    }
+  } else {
+    return next(
+      error(400, "Cannot locate folder or cloud storage instructions")
+    );
+  }
+
+  if (download_files) {
+    var bucket_uri = `s3://${bucket_name}/${host}/${group}/${repository}/${folder_path}`
+    var command = `aws s3 sync ${bucket_uri} ${folder}`;
+    logger("post", "gitclone/files", `Executing command ${command}`, indent=1);
+
+    // Run the server commands
+    const child = spawn(command, {
+      shell: true,
+    });
+
+    child.stdout.on("data", (data) => {
+      db.query("UPDATE clonestatus SET message = $1 WHERE id = $2", [
+        data,
+        status_id,
+      ]);
+      log.push(`stdout: ${data}`);
+      logger("post", "gitclone/files", `stdout: ${data}`, (indent = 2));
+    });
+    child.stderr.on("data", (data) => {
+      db.query("UPDATE clonestatus SET message = $1 WHERE id = $2", [
+        data,
+        status_id,
+      ]);
+      log.push(`stdout: ${data}`);
+      console.error(`stdout: ${data}`);
+    });
+    child.on("error", async (data) => {
+      await db.query(
+        "UPDATE clonestatus SET status = $1, message = $2 WHERE id = $3",
+        ["failed", log, status_id]
+      );
+    });
+    child.on("exit", async (data) => {
+      try {
+        if (data === 0) {
+          logger(
+            "post",
+            "gitclone/files",
+            "Sync completed successfully.",
+            (indent = 1)
+          );
+          await db.query(
+            "UPDATE clonestatus SET message = $1, status = $2 WHERE id = $3",
+            ["Sync completed successfully", "succeeded", status_id]
+          );
+        } else {
+          await db.query(
+            "UPDATE clonestatus SET status = $1, message = $2 WHERE id = $3",
+            ["failed", log, status_id]
+          );
+        }
+      } catch (error) {
+        console.log(error.message);
+        await db.query(
+          "UPDATE clonestatus SET status = $1, message = $2 WHERE id = $3",
+          ["failed", error.message, status_id]
+        );
+      }
+    });
+  }
+});
+
 getListingS3 = (prefix) => {
   return new Promise((resolve, reject) => {
     try {
@@ -437,4 +449,5 @@ getListingS3 = (prefix) => {
     }
   });
 };
+
 module.exports = router;
