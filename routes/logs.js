44618fd7440fafc47a50da21c3a258185fa785db
JamesRunnalls <james.runnalls@eawag.ch>
2023-02-02 11:47:03 +0100

    Catch error log

--- routes/logs.js
+++ routes/logs.js
@@ -3,7 +3,7 @@ const router = new Router();
 const { error, isInt } = require("../functions");
 const { exec } = require("child_process");
 
-router.get("/:rows", async (req, res, next) => {
+router.get("/info/:rows", async (req, res, next) => {
   var rows = req.params.rows;
   if (!isInt(rows)) {
     return next(error(400, "Rows must be an integer"));
@@ -20,5 +20,23 @@ router.get("/:rows", async (req, res, next) => {
     return next(error(500, "Failed to access logs"));
   }
 });
+
+router.get("error/:rows", async (req, res, next) => {
+    var rows = req.params.rows;
+    if (!isInt(rows)) {
+      return next(error(400, "Rows must be an integer"));
+    }
+    try {
+      exec(
+        `tail -n ${rows} ~/.pm2/logs/datalakes-error.log`,
+        (error, stdout, stderr) => {
+          res.set({"Content-Disposition":"attachment; filename=\"log.txt\""});
+          res.status(200).send(stdout);
+        }
+      );
+    } catch (e) {
+      return next(error(500, "Failed to access logs"));
+    }
+  });
 
 module.exports = router;
