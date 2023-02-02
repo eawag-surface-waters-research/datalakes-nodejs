82eeb7271a35bd00d43e61d823b387aff5ebc402
JamesRunnalls <james.runnalls@eawag.ch>
2023-02-02 10:59:19 +0100

    Fix bug in timestamp

--- functions.js
+++ functions.js
@@ -10,11 +10,11 @@ const { spawn } = require("child_process");
 now = () => {
   let d = new Date();
   let date = ("0" + d.getDate()).slice(-2);
-  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-  let year = date_ob.getFullYear();
-  let hours = date_ob.getHours();
-  let minutes = date_ob.getMinutes();
-  let seconds = date_ob.getSeconds();
+  let month = ("0" + (d.getMonth() + 1)).slice(-2);
+  let year = d.getFullYear();
+  let hours = d.getHours();
+  let minutes = d.getMinutes();
+  let seconds = d.getSeconds();
   return (
     year +
     "-" +
