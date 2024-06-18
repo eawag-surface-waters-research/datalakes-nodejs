cc0a743efd7e600b66b854f86f3bd5a703d236d0
James Runnalls <runnalls.james@gmail.com>
2024-06-18 09:34:06 +0200

    Fix data endpoint bug

--- routes/data.js
+++ routes/data.js
@@ -45,18 +45,25 @@ router.get("/:dataset_id/:parameter", async (req, res, next) => {
     }
   }
   if (isNaN(timestamp)) {
-    return next(error(400, "Invalid date format, needs to be ISO-8601 and the format is: YYYY-MM-DDTHH:mm:ss.sssZ"));
+    return next(
+      error(
+        400,
+        "Invalid date format, needs to be ISO-8601 and the format is: YYYY-MM-DDTHH:mm:ss.sssZ"
+      )
+    );
   }
-
   const query = `
     SELECT *
     FROM files
-    WHERE mindatetime <= $2
-    AND maxdatetime >= $2
-    AND datasets_id = $1
-    AND filetype = 'json'
+    WHERE 
+        (mindatetime <= $2
+        AND maxdatetime >= $2
+        AND datasets_id = $1
+        AND filetype = 'json')
     OR (
-        NOT EXISTS (
+        datasets_id = $1
+        AND filetype = 'json'
+        AND NOT EXISTS (
             SELECT 1
             FROM files
             WHERE mindatetime <= $2
@@ -87,55 +94,53 @@ router.get("/:dataset_id/:parameter", async (req, res, next) => {
   if (rows.length < 1) {
     return next(error(404, "Dataset not found in database"));
   }
-  var {
-    rows: parameters,
-  } = await db.query(
+  var { rows: parameters } = await db.query(
     "SELECT * FROM datasetparameters WHERE datasets_id = $1 AND parameters_id = $2",
     [id, 1]
   );
   if (parameters.length < 1) {
     return next(error(404, "Time variable not found for dataset"));
   }
-  var axis = parameters[0].axis
+  var axis = parameters[0].axis;
   var link = rows[0].filelink;
   var data = await readFileAsync(
     __dirname.replace("routes", "") + link,
     "utf8"
   );
-  data = JSON.parse(data)
-  var time = data[axis]
-  var index = findClosestDateIndex(time, timestamp)
+  data = JSON.parse(data);
+  var time = data[axis];
+  var index = findClosestDateIndex(time, timestamp);
   var dt = new Date(time[index] * 1000);
 
   if (data[parameter] === undefined) {
     return next(error(404, "Invalid parameter please select another"));
   }
-  var array = data[parameter]
+  var array = data[parameter];
   var value;
-  if (Array.isArray(array[0])){
+  if (Array.isArray(array[0])) {
     return next(error(404, "Endpoint is only valid for 1D outputs"));
   } else {
-    value = data[parameter][index]
+    value = data[parameter][index];
   }
-  res.status(200).send({time: dt, value: value});
+  res.status(200).send({ time: dt, value: value });
 });
 
 findClosestDateIndex = (datesArray, inputDate) => {
-    const inputTimestamp = inputDate.getTime() / 1000;
-    
-    let minDifference = Infinity;
-    let closestIndex = -1;
-  
-    for (let i = 0; i < datesArray.length; i++) {
-      const timestamp = datesArray[i];
-      const difference = Math.abs(timestamp - inputTimestamp);
-      
-      if (difference < minDifference) {
-        minDifference = difference;
-        closestIndex = i;
-      }
+  const inputTimestamp = inputDate.getTime() / 1000;
+
+  let minDifference = Infinity;
+  let closestIndex = -1;
+
+  for (let i = 0; i < datesArray.length; i++) {
+    const timestamp = datesArray[i];
+    const difference = Math.abs(timestamp - inputTimestamp);
+
+    if (difference < minDifference) {
+      minDifference = difference;
+      closestIndex = i;
     }
-    return closestIndex;
   }
+  return closestIndex;
+};
 
 module.exports = router;
