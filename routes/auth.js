a35d9e9bf38ea83066eaa7a4e86d0552ed97dad6
James Runnalls <runnalls.james@gmail.com>
2025-10-17 11:36:46 +0200

    Revert changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 const Router = require("express-promise-router");
 const router = new Router();
 const axios = require("axios");
-const querystring = require("querystring");
 
 // Step 2: GitHub redirects back here with a code
 router.post("/github/token", async (req, res) => {
@@ -10,17 +9,14 @@ router.post("/github/token", async (req, res) => {
   try {
     // Step 3: Exchange code for access token
     const tokenRes = await axios.post(
-      "https://github.com/login/oauth/access_token",
-      querystring.stringify({
+      'https://github.com/login/oauth/access_token',
+      {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
         code,
-      }),
+      },
       {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
+        headers: { Accept: 'application/json' },
       }
     );
 
